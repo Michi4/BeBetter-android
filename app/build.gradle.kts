81cb2c976bf3e8ddb00b86dd2fdaea02a50169ca
Michi4 <michael.ruep@gmail.com>
2026-09-15 09:43:14 +0200

    chore: bump versionCode 4 - mobile history & create 1:1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "at.websters.bebetter"
         minSdk = 28
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
 
         vectorDrawables {
